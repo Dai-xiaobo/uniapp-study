c1f26f0475f2ebeedca935b8b278c8223e185ca5
XiaoBo.Dai <xiaobo.dai@hand-china.com>
2020-06-20 17:40:52 +0800

    feat:xxx

---
+++ components/webview.vue
@@ -0,0 +1,41 @@
+<template>
+	<view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props:{
+			title:{
+				type:String,
+				default:''
+			},
+			url:{
+				type:String,
+				default:''
+			},
+		},
+		data(){
+			return {
+				webviewStyles: {
+				                    progress: {
+				                        color: '#FF3333'
+				                    }
+				                },
+				src:''
+			}
+		},
+		onLoad(opt) {
+			this.src = opt['u'];
+			this.title = opt['title'];
+			
+			uni.setNavigationBarTitle({
+				title: this.title
+			});
+		}
+		
+	}
+</script>
+
+<style>
+</style>
